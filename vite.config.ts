7f2342e9a135cbb65ec0e4b847f61e64defb96bf
pipecraft <pipecraft.net@gmail.com>
2025-04-17 10:49:54 +0800

    update icon url

--- vite.config.ts
+++ vite.config.ts
@@ -18,13 +18,8 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/logo-192x192.png',
+            src: '/android-icon-192x192.png',
             sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/logo-512x512.png',
-            sizes: '512x512',
             type: 'image/png',
           },
         ],
